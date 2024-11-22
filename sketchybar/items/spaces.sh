0b1ee583b7546cb864d3ad32c2e98a09f1bc4a18
Jakub Mazur <jakmaz.dev@icloud.com>
2024-11-22 01:11:20 +0100

    feat: front app working (spaces are not yet implemented)

--- sketchybar/items/spaces.sh
+++ sketchybar/items/spaces.sh
@@ -6,6 +6,8 @@ for sid in $(aerospace list-workspaces --all); do
   sketchybar --add item space.$sid left \
     --subscribe space.$sid aerospace_workspace_change \
     --set space.$sid \
+    icon=$sid \
+    label.font="sketchybar-app-font:Regular:16.0" \
     background.color=0x44ffffff \
     background.corner_radius=5 \
     background.height=20 \
@@ -14,3 +16,12 @@ for sid in $(aerospace list-workspaces --all); do
     click_script="aerospace workspace $sid" \
     script="$CONFIG_DIR/plugins/space.sh $sid"
 done
+
+sketchybar --add item space_separator left \
+  --set space_separator icon="" \
+  icon.color=$ACCENT_COLOR \
+  icon.padding_left=4 \
+  label.drawing=off \
+  background.drawing=off \
+  script="$PLUGIN_DIR/space_windows.sh" \
+  --subscribe space_separator space_windows_change
