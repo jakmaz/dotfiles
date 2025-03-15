49199a5708fea6399b25ad456f6f47ac57d18f1e
Jakub Mazur <jakmaz.dev@icloud.com>
2025-03-16 00:38:45 +0100

    feat: update alpha dashboard

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -15,5 +15,15 @@ return {
 	         [ @jakmaz ]
     ]]
     opts.section.header.val = vim.split(logo, "\n", { trimempty = true })
+    opts.section.buttons.val = {
+      opts.button("e", " " .. " Explore files", "<cmd> Neotree <cr>"),
+      opts.button("r", " " .. " Recent files", [[<cmd> lua LazyVim.pick("oldfiles")() <cr>]]),
+      opts.button("f", " " .. " Find files", "<cmd> lua LazyVim.pick()() <cr>"),
+      opts.button("g", " " .. " Find text", [[<cmd> lua LazyVim.pick("live_grep")() <cr>]]),
+      opts.button("n", " " .. " New file", [[<cmd> ene <BAR> startinsert <cr>]]),
+      opts.button("s", " " .. " Restore Session", [[<cmd> lua require("persistence").load() <cr>]]),
+      opts.button("l", "󰒲 " .. " Lazy", "<cmd> Lazy <cr>"),
+      opts.button("q", " " .. " Quit", "<cmd> qa <cr>"),
+    }
   end,
 }
