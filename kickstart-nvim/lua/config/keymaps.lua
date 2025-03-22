82dae7f21094959cb11ba9e965760ca236d5cbf1
Jakub Mazur <jakmaz.dev@icloud.com>
2025-03-22 22:04:11 +0100

    feat: add yank all keymap

--- kickstart-nvim/lua/config/keymaps.lua
+++ kickstart-nvim/lua/config/keymaps.lua
@@ -6,6 +6,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Quit neovim
 vim.keymap.set('n', '<leader>q', '<cmd>qa<CR>', { desc = 'Quit all' })
 
+-- Yank whole text
+vim.keymap.set('n', '<leader>cy', ':%y+<CR>', { desc = 'Yank entire buffer to clipboard' })
+
 -- Keybinds to make split navigation easier.
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
