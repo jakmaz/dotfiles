5e177eecf15ef5e89d95ec334f7e4e2295754249
Jakub Mazur <jakmaz.dev@icloud.com>
2025-04-04 21:07:19 +0200

    feat: add lspconfig and mason in place of native lsp configuration

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,6 @@
 require 'config.options'
 require 'config.keymaps'
 require 'config.autocmds'
-require 'config.lspconfig'
 require 'config.diagnostics'
 
 -- Install `lazy.nvim` plugin manager
---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,66 @@
+return {
+  -- Mason: installs and manages external tools like LSP servers
+  {
+    'williamboman/mason.nvim',
+    opts = {},
+  },
+
+  -- Mason-LSPConfig: tells Mason which servers to install and links them to lspconfig
+  {
+    'williamboman/mason-lspconfig.nvim',
+    opts = {
+      ensure_installed = {
+        'lua_ls',        -- Lua (great for editing Neovim config)
+        'pyright',       -- Python
+        'ts_ls',         -- TypeScript / JavaScript
+        'rust_analyzer', -- Rust
+        'clangd',        -- C / C++
+        'tailwindcss',   -- Tailwind CSS
+        'marksman',      -- Markdown
+      },
+    },
+  },
+
+  -- nvim-lspconfig: connects Neovim to installed LSP servers
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      local lspconfig = require 'lspconfig'
+
+      -- Lua (for Neovim configuration)
+      lspconfig.lua_ls.setup {
+        settings = {
+          Lua = {
+            runtime = { version = 'LuaJIT' },
+            diagnostics = {
+              globals = { 'vim' },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = { enable = false },
+          },
+        },
+      }
+
+      -- Python
+      lspconfig.pyright.setup {}
+
+      -- JavaScript / TypeScript
+      lspconfig.ts_ls.setup {}
+
+      -- Rust
+      lspconfig.rust_analyzer.setup {}
+
+      -- C / C++
+      lspconfig.clangd.setup {}
+
+      -- Tailwind CSS
+      lspconfig.tailwindcss.setup {}
+
+      -- Markdown
+      lspconfig.marksman.setup {}
+    end,
+  },
+}
