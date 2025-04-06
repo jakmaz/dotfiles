3f9d1b9194161725b439a753f8036cc882ac0dae
Jakub Mazur <jakmaz.dev@icloud.com>
2025-04-06 09:22:21 +0200

    feat: add code action, rename, select entire buffer

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -11,8 +11,16 @@ vim.keymap.set('n', '<leader>q', '<cmd>qa<CR>', { desc = 'Quit All' })
 -- Yank whole text
 vim.keymap.set('n', '<leader>cy', ':%y+<CR>', { desc = 'Yank Entire Buffer' })
 
+-- Select whole text
+vim.keymap.set("n", "<leader>cs", "ggVG", { desc = "Select Entire Buffer" })
+
+
 -- Keybinds to make split navigation easier.
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- Lsp actions
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
