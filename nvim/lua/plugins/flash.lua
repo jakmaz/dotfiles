639cfb7ed1bc3aa7f2bdf93ceae6b95df9897ca6
Jakub Mazur <jakmaz.dev@icloud.com>
2025-04-21 12:36:28 +0200

    feat: put flash on enter key, change labels for colemak-dh and make them visible through changing the color

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,14 +1,24 @@
 return {
   'folke/flash.nvim',
   event = 'VeryLazy',
-  ---@type Flash.Config
-  opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<CR>",  mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+    { "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+    { "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+    { "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
   },
+  config = function()
+    -- Load your colorscheme FIRST (if not already loaded globally)
+    -- vim.cmd("colorscheme tokyonight") -- Replace with your colorscheme, if needed
+
+    -- Configure Flash labels
+    require("flash").setup {
+      labels = "arstgmneio"
+    }
+
+    -- Now, customize Flash colors
+    vim.api.nvim_set_hl(0, "FlashLabel", { fg = "#ffffff", bold = true })
+  end,
 }
