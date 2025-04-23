78bb205243e3a68887bc1d30b9edc2982426bf4e
Jakub Mazur <jakmaz.dev@icloud.com>
2025-04-23 22:48:02 +0200

    feat: lazy load the plugin

--- nvim/lua/plugins/ts-autotag.lua
+++ nvim/lua/plugins/ts-autotag.lua
@@ -1,1 +1,7 @@
-return { 'windwp/nvim-ts-autotag', opts = {} }
+return {
+  'windwp/nvim-ts-autotag',
+  ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  config = function()
+    require('nvim-ts-autotag').setup()
+  end,
+}
