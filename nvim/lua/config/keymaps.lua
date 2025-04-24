fe5188c3c2dca8eb27350456b1d0100d512e7ee0
Jakub Mazur <jakmaz.dev@icloud.com>
2025-04-24 20:49:34 +0200

    feat: ctrl+s also works in insert mode

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -8,14 +8,14 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Quit neovim
 vim.keymap.set('n', '<leader>q', '<cmd>qa<CR>', { desc = 'Quit All' })
 
--- Save on Ctrl+S
-vim.keymap.set({ 'n', 'i' }, '<C-s>', '<cmd>w<CR>', { desc = 'Save File' })
+-- Save on Ctrl+S in normal and insert mode, then return to normal mode
+vim.keymap.set({ 'n', 'i' }, '<C-s>', '<cmd>w<CR><Esc>', { desc = 'Save File' })
 
 -- Yank whole text
 vim.keymap.set('n', '<leader>cy', ':%y+<CR>', { desc = 'Yank Entire Buffer' })
 
 -- Select whole text
-vim.keymap.set("n", "<leader>cs", "ggVG", { desc = "Select Entire Buffer" })
+vim.keymap.set('n', '<leader>cs', 'ggVG', { desc = 'Select Entire Buffer' })
 
 -- Keybinds to make split navigation easier.
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
@@ -24,45 +24,40 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Lsp actions
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action' })
+vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = 'Rename' })
 
 -- Function to copy all diagnostics to the clipboard
 local function copy_all_diagnostics()
   local diagnostics = vim.diagnostic.get(nil) -- get all diagnostics in current buffer
   if vim.tbl_isempty(diagnostics) then
-    vim.notify("No diagnostics to copy!", vim.log.levels.INFO)
+    vim.notify('No diagnostics to copy!', vim.log.levels.INFO)
     return
   end
 
   local lines = {}
   for _, d in ipairs(diagnostics) do
     local msg = string.format(
-      "[%s] %s:%d:%d: %s",
+      '[%s] %s:%d:%d: %s',
       vim.diagnostic.severity[d.severity]:sub(1, 1),
       vim.api.nvim_buf_get_name(0),
       d.lnum + 1,
       d.col + 1,
-      d.message:gsub("\n", " ")
+      d.message:gsub('\n', ' ')
     )
     table.insert(lines, msg)
   end
 
-  local text = table.concat(lines, "\n")
-  vim.fn.setreg("+", text) -- copy to system clipboard
-  vim.notify("Diagnostics copied to clipboard!", vim.log.levels.INFO)
+  local text = table.concat(lines, '\n')
+  vim.fn.setreg('+', text) -- copy to system clipboard
+  vim.notify('Diagnostics copied to clipboard!', vim.log.levels.INFO)
 end
 
 -- Keymap: <leader>cd to copy all diagnostics
-vim.keymap.set(
-  "n",
-  "<leader>cd",
-  copy_all_diagnostics,
-  { desc = "Copy All Diagnostics" }
-)
+vim.keymap.set('n', '<leader>cd', copy_all_diagnostics, { desc = 'Copy All Diagnostics' })
 
 -- Center screen after scrolling or searching (from prime)
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result centered" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result centered" })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll down and center' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll up and center' })
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next search result centered' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous search result centered' })
