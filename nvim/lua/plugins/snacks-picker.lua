0d4471ce53c2ed6597eaf73c9575430928575832
Jakub Mazur <jakmaz.dev@icloud.com>
2025-05-17 18:08:17 +0200

    feat: make explorer centered and add relative lines
    based on:
    https://github.com/folke/snacks.nvim/issues/1385#issuecomment-2676672884
    https://www.reddit.com/r/neovim/comments/1kbqsdc/snacks_explorer_preview_to_the_right/

--- nvim/lua/plugins/snacks-picker.lua
+++ nvim/lua/plugins/snacks-picker.lua
@@ -5,10 +5,35 @@ return {
       enabled = true,
       sources = {
         explorer = {
-          layout = { auto_hide = { 'input' } },
-          win = { list = { keys = { ['<Right>'] = 'confirm', ['<Left>'] = 'explorer_close' } } },
+          layout = {
+            { preview = true },
+            layout = {
+              box = 'horizontal',
+              width = 0.8,
+              height = 0.8,
+              {
+                box = 'vertical',
+                border = 'rounded',
+                title = '{source} {live} {flags}',
+                title_pos = 'center',
+                { win = 'input', height = 1, border = 'bottom' },
+                { win = 'list', border = 'none' },
+              },
+              { win = 'preview', border = 'rounded', width = 0.7, title = '{preview}' },
+            },
+          },
+          win = {
+            list = {
+              keys = { ['<Right>'] = 'confirm', ['<Left>'] = 'explorer_close' },
+              wo = {
+                number = true, -- Enable line numbers
+                relativenumber = true, -- Enable relative line numbers
+              },
+            },
+          },
         },
       },
     },
   },
 }
+
