03ef1d4975e7ba043b477f2098c4d189b6001d08
Jakub Mazur <jakmaz.dev@icloud.com>
2025-05-17 21:50:39 +0200

    fix: exiting nvim no longer closes its window

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+# Check if an argument is provided
 if [[ $# -eq 1 ]]; then
-  selected="$HOME/git/$1" # Ensure full path if argument is passed
+  selected="$HOME/git/$1" # Use full path if argument is passed
 else
+  # Use fzf to select a project directory
   selected=$(
-    find ~/git -mindepth 1 -maxdepth 1 -type d -print |
-      sed "s|$HOME/git/||" | # Show only project names in fzf
+    find ~/git -mindepth 1 -maxdepth 1 -type d -print | 
+      sed "s|$HOME/git/||" | # Display only project names in fzf
       fzf \
         --margin=2,10% \
         --border=rounded \
@@ -17,24 +19,33 @@ else
   selected="$HOME/git/$selected" # Convert back to full path
 fi
 
+# Exit if no selection or the selection is not a directory
 if [[ -z $selected || ! -d $selected ]]; then
   exit 0
 fi
 
+# Generate a session name from the selected directory
 selected_name=$(basename "$selected" | tr . _)
+
+# Check if tmux is running
 tmux_running=$(pgrep tmux)
 
+# If not inside TMUX and tmux is not running, start a new session
 if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-  tmux new-session -s "$selected_name" -c "$selected" -n "nvim" "nvim"
+  tmux new-session -s "$selected_name" -c "$selected" -n "nvim" \
+    "nvim; nu" # Keep the pane open and start nushell after nvim exits
   tmux new-window -t "$selected_name":1 -n "terminal" -c "$selected"
   tmux select-window -t "$selected_name":0
   exit 0
 fi
 
+# Create a new session if it does not already exist
 if ! tmux has-session -t="$selected_name" 2>/dev/null; then
-  tmux new-session -ds "$selected_name" -c "$selected" -n "  nvim" "nvim"
+  tmux new-session -ds "$selected_name" -c "$selected" -n "  nvim" \
+    "nvim; nu" # Keep the pane open and start nushell after nvim exits
   tmux new-window -t "$selected_name":1 -n "  terminal" -c "$selected"
   tmux select-window -t "$selected_name":0
 fi
 
+# Switch to the session
 tmux switch-client -t "$selected_name"
