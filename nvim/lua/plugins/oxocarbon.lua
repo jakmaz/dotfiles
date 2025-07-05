83211885002d2987516d382a4c75345babcadd27
Jakub Mazur <jakmaz.dev@icloud.com>
2025-07-05 13:06:55 +0200

    feat: remove oxocarbon commit lock

--- nvim/lua/plugins/oxocarbon.lua
+++ nvim/lua/plugins/oxocarbon.lua
@@ -1,6 +1,5 @@
 return {
   'nyoom-engineering/oxocarbon.nvim',
-  commit = '8f22819',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function() vim.cmd.colorscheme 'oxocarbon' end,
 }
