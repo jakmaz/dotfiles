c224ef5baaec68cd5cea950b661fe7aabcac913d
Jakub Mazur <jakmaz.dev@icloud.com>
2025-07-07 14:18:58 +0200

    feat: update key analyzer layout and assign command

--- nvim/lua/plugins/key-analyzer.lua
+++ nvim/lua/plugins/key-analyzer.lua
@@ -1,7 +1,8 @@
 return {
   'meznaric/key-analyzer.nvim',
+  keys = { { '<leader>uk', '<Cmd>KeyAnalyzer leader<CR>', desc = 'Key Analyzer' } },
   opts = {
-    layout = 'colemak-dh',
+    layout = 'colemak_dh',
     promotion = false,
   },
 }
