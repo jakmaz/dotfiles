13faaf1a3f5f34489825b97f3eeee9a1fefafd70
Jakub Mazur <jakmaz.dev@icloud.com>
2025-07-08 19:15:47 +0200

    feat: add autocommand to refresh buffer content when changing focus (good for working with agents)

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,7 +1,12 @@
+-- Highlight on yank (your current autocommand)
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+  callback = function() vim.highlight.on_yank() end,
+})
+
+-- Auto-reload files changed outside of Neovim
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter' }, {
+  desc = 'Auto check for file changes on focus or buffer enter',
+  callback = function() vim.cmd 'checktime' end,
 })
