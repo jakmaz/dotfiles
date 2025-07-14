ef4ca26a50d2acfc6ad64bf8ba247435da6f9d57
Jakub Mazur <jakmaz.dev@icloud.com>
2025-07-14 23:30:51 +0200

    feat: add apple music instead of spotify

---
+++ sketchybar/items/apple_music.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Initialization
+sketchybar --add item apple_music center
+
+# Config
+APPLE_MUSIC_EVENT="com.apple.iTunes.playerInfo"
+sketchybar --add event apple_music_change $APPLE_MUSIC_EVENT
+sketchybar --set apple_music icon=󰝚 
+sketchybar --set apple_music script="$PLUGIN_DIR/apple_music.sh"
+sketchybar --subscribe apple_music apple_music_change
+
+# Overrides of default settings
+sketchybar --set apple_music background.color=0x00000000
---
+++ sketchybar/plugins/apple_music.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+INFO=$(osascript -e '
+  tell application "Music"
+    if it is running and player state is playing then
+      set trackName to name of current track
+      set artistName to artist of current track
+      return trackName & " – " & artistName
+    else
+      return ""
+    end if
+  end tell
+')
+
+if [ -z "$INFO" ]; then
+  sketchybar --set "$NAME" icon="" label="[jakmaz] "
+else
+  sketchybar --set "$NAME" label="$INFO" drawing=on
+fi
