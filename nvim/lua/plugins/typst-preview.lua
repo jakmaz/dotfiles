f2a5e8bde503903be1d7a1b719722973e0d6eb20
Jakub Mazur <jakmaz.dev@icloud.com>
2025-07-17 22:51:02 +0200

    feat: add typst keymaps

--- nvim/lua/plugins/typst-preview.lua
+++ nvim/lua/plugins/typst-preview.lua
@@ -2,5 +2,9 @@ return {
   'chomosuke/typst-preview.nvim',
   ft = 'typst',
   version = '1.*',
+  keys = {
+    { '<leader>tp', '<cmd>TypstPreview<cr>', desc = 'Typst Preview' },
+    { '<leader>tP', '<cmd>TypstPreviewStop<cr>', desc = 'Typst Preview Stop' },
+  },
   opts = {}, -- lazy.nvim will implicitly calls `setup {}`
 }
