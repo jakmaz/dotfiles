1f1f1b05283cd7522adc61bd65f4acfdb45fb8b8
Jakub Mazur <jakmaz.social@icloud.com>
2026-02-16 12:47:04 +0100

    feat: blackjack and wrapped

---
+++ nvim/lua/plugins/blackjack.lua
@@ -0,0 +1,7 @@
+return {
+  keys = {
+    { '<leader>ob', '<cmd>BlackJackNewGame<cr>', desc = 'Blackjack' },
+  },
+  'alanfortlink/blackjack.nvim',
+  requires = { 'nvim-lua/plenary.nvim' },
+}
---
+++ nvim/lua/plugins/wrapped.lua
@@ -0,0 +1,8 @@
+return {
+  'aikhe/wrapped.nvim',
+  dependencies = { 'nvzone/volt' },
+  keys = {
+    { '<leader>ow', '<cmd>NvimWrapped<cr>', desc = 'Wrap' },
+  },
+  opts = {},
+}
