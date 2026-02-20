59f17d3d0548e3eef499146e8688fb1d6d15f27b
Jakub Mazur <jakmaz.social@icloud.com>
2026-02-20 13:14:24 +0100

    chore: change paths, add new colorscheme for reference

--- nvim/lua/plugins/oxide.lua
+++ nvim/lua/plugins/oxide.lua
@@ -1,5 +1,5 @@
 return {
-  dir = '/Users/jakubmazur/git/oxide.nvim',
+  dir = '/Users/jakubmazur/git/oxide/nvim',
   name = 'oxide.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function() vim.cmd.colorscheme 'oxide' end,
--- nvim/lua/plugins/oxocarbon.lua
+++ nvim/lua/plugins/oxocarbon.lua
@@ -1,5 +1,5 @@
 return {
-  dir = '/Users/jakubmazur/git/oxocarbon.nvim',
+  'nyoom-engineering/oxocarbon.nvim',
   name = 'oxocarbon.nvim',
   priority = 1000, -- Make sure to load this before all the other start plugins.
 }
