f149ce7cbc4ff37b2ec241f40b25f5eca81dd489
Jakub Mazur <jakmaz.social@icloud.com>
2026-03-24 12:54:30 +0100

    style: remove front app animations

--- sketchybar/items/front_app.sh
+++ sketchybar/items/front_app.sh
@@ -3,6 +3,6 @@
 sketchybar --add item front_app left \
     --set front_app \
     icon.font="sketchybar-app-font:Regular:16.0" \
-    icon.left_padding=20 \
+    left_padding=20 \
     script="$PLUGIN_DIR/front_app.sh" \
     --subscribe front_app front_app_switched
