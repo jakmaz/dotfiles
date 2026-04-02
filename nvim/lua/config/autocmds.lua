dac57fc2003b49dbe6280c50dcbd737cc39857bc
Jakub Mazur <jakmaz.social@icloud.com>
2026-04-02 19:19:58 +0200

    feat: substitute plugin settings and cleanup autocmds

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,8 +1,8 @@
 -- Highlight on yank (your current autocommand)
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function() vim.highlight.on_yank() end,
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function() vim.highlight.on_yank { timeout = 100 } end,
 })
 
 -- Auto-reload files changed outside of Neovim
@@ -12,9 +12,7 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold' }, {
   group = autoread_group,
   desc = 'Auto check for file changes',
   callback = function()
-    if vim.fn.mode() ~= 'c' then
-      vim.cmd('checktime')
-    end
+    if vim.fn.mode() ~= 'c' then vim.cmd 'checktime' end
   end,
 })
 
@@ -22,26 +20,5 @@ vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold' }, {
 vim.api.nvim_create_autocmd('FileChangedShellPost', {
   group = autoread_group,
   desc = 'Notify when file changed on disk',
-  callback = function()
-    vim.notify('File changed on disk. Buffer reloaded.', vim.log.levels.INFO)
-  end,
-})
-
--- Open help pages as buffers instead of splits
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'help',
-  desc = 'Open help pages in current buffer instead of split',
-  callback = function()
-    -- Make help buffer listed so it shows in buffer picker
-    vim.bo.buflisted = true
-    
-    -- Only apply if help opened in a split (winnr > 1 means multiple windows)
-    if vim.fn.winnr('$') > 1 then
-      -- Move the help buffer to the main window
-      local help_buf = vim.api.nvim_get_current_buf()
-      vim.cmd('wincmd p') -- Go to previous window (main window)
-      vim.api.nvim_set_current_buf(help_buf) -- Set help buffer in main window
-      vim.cmd('helpclose') -- Close the help split
-    end
-  end,
+  callback = function() vim.notify('File changed on disk. Buffer reloaded.', vim.log.levels.INFO) end,
 })
--- nvim/plugin/substitute.lua
+++ nvim/plugin/substitute.lua
@@ -1,13 +1,21 @@
-vim.pack.add({ 'https://github.com/gbprod/substitute.nvim' })
+vim.pack.add { 'https://github.com/gbprod/substitute.nvim' }
 
-require('substitute').setup({})
-
+require('substitute').setup {
+  highlight_substituted_text = {
+    timer = 100,
+  },
+}
 vim.keymap.set('n', 's', require('substitute').operator, { noremap = true, desc = 'Substitute' })
 vim.keymap.set('n', 'ss', require('substitute').line, { noremap = true, desc = 'Substitute line' })
-vim.keymap.set('n', 'S', require('substitute').eol, { noremap = true, desc = 'Substitute to end of line' })
+vim.keymap.set('n', 's', require('substitute').operator, { noremap = true, desc = 'Substitute' })
 vim.keymap.set('x', 's', require('substitute').visual, { noremap = true, desc = 'Substitute selection' })
 
 vim.keymap.set('n', '<leader>rx', require('substitute.exchange').operator, { noremap = true, desc = 'Exchange' })
 vim.keymap.set('n', '<leader>rxx', require('substitute.exchange').line, { noremap = true, desc = 'Exchange line' })
-vim.keymap.set('x', '<leader>rX', require('substitute.exchange').visual, { noremap = true, desc = 'Exchange selection' })
+vim.keymap.set(
+  'x',
+  '<leader>rX',
+  require('substitute.exchange').visual,
+  { noremap = true, desc = 'Exchange selection' }
+)
 vim.keymap.set('n', '<leader>rxc', require('substitute.exchange').cancel, { noremap = true, desc = 'Cancel exchange' })
