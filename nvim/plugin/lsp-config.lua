e07ad5104640de911f0ea43c6e968fc4ffc4bf4e
Jakub Mazur <jakmaz.social@icloud.com>
2026-04-07 23:07:47 +0200

    feat: use ruff for python instead of pyright

--- nvim/plugin/lsp-config.lua
+++ nvim/plugin/lsp-config.lua
@@ -1,14 +1,14 @@
-vim.pack.add({ 'https://github.com/williamboman/mason.nvim' }) -- 'mason-org/mason.nvim' redirects to williamboman
-vim.pack.add({ 'https://github.com/williamboman/mason-lspconfig.nvim' })
-vim.pack.add({ 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' })
-vim.pack.add({ 'https://github.com/neovim/nvim-lspconfig' })
+vim.pack.add { 'https://github.com/williamboman/mason.nvim' } -- 'mason-org/mason.nvim' redirects to williamboman
+vim.pack.add { 'https://github.com/williamboman/mason-lspconfig.nvim' }
+vim.pack.add { 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' }
+vim.pack.add { 'https://github.com/neovim/nvim-lspconfig' }
 
-require('mason').setup({})
+require('mason').setup {}
 
-require('mason-lspconfig').setup({
+require('mason-lspconfig').setup {
   ensure_installed = {
     'lua_ls',
-    'pyright',
+    'ruff',
     'eslint',
     'rust_analyzer',
     'clangd',
@@ -30,9 +30,9 @@ require('mason-lspconfig').setup({
       'harper_ls',
     },
   },
-})
+}
 
-require('mason-tool-installer').setup({
+require('mason-tool-installer').setup {
   ensure_installed = {
     'tflint',
     'trivy',
@@ -54,7 +54,7 @@ require('mason-tool-installer').setup({
   },
   auto_update = false,
   run_on_start = true,
-})
+}
 
 -- The user configuration sets lsp.config which is part of nvim-lspconfig setup natively
 vim.lsp.config('harper_ls', {
